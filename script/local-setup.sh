05ca27f06156e6140dc38fa6d263a05eb39f6555
indramhaarta <indramhrt@gmail.com>
2024-06-14 21:45:11 +0700

    feat: handle auth

---
+++ script/local-setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+echo "Start to execute script"
+echo "============================================================"
+
+# docker-compose down
+
+copy_env() {
+    local dir=$1
+    local env_file_name=${2:-".env"}
+
+    if [ -d "$dir" ]; then
+        echo "Directory '$dir' exists. Checking if it's a Git repository..."
+    else
+        echo "Directory '$dir' does not exist. Cloning repository..."
+    fi
+
+    cp .env "./$dir/$env_file_name"
+    echo "Environment file copied to $dir/$env_file_name"
+    echo "'$dir': Done"
+    echo "============================================================"
+}
+
+declare -a repos=( 
+    "frontend|.env"
+    "backend|.env"
+)
+
+for repo in "${repos[@]}"; do
+    IFS='|' read -r dir repo_url env_file_name <<< "$repo"
+    copy_env "$dir" "$repo_url" "$env_file_name"
+done
+
+if command -v docker >/dev/null 2>&1; then
+    echo "Docker is installed."
+else
+    echo "Docker is not installed. Please install :)"
+    exit 1
+fi
+
+if command -v docker-compose >/dev/null 2>&1; then
+    echo "Docker Compose is installed."
+else
+    echo "Docker Compose is not installed. Please install; :)"
+    exit 1
+fi
+
+docker-compose up -d --build
+docker image prune -f
