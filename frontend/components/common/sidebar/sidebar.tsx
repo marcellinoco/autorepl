1c3d8a95e25b7de851d9c2c60da4a9693069da49
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-06-14 22:32:52 +0700

    fix path auth on sidebar

--- frontend/components/common/sidebar/sidebar.tsx
+++ frontend/components/common/sidebar/sidebar.tsx
@@ -2,7 +2,7 @@
 
 import { logout } from "@/app/auth/action";
 import useAuthStore from "@/store/useAuthStore";
-import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { FC, useState } from "react";
 
 const Sidebar: FC = () => {
@@ -20,6 +20,11 @@ const Sidebar: FC = () => {
     setSelectedStatus(status);
     console.log(`Selected Status: ${status}`);
   };
+
+  // If the path includes "/auth", return an empty fragment
+  if (pathname.includes("/auth")) {
+    return <></>;
+  }
 
   return (
     <div
