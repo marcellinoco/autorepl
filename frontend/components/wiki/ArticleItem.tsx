2c73f0b311273c3c4eb415b10660eec7eaa20742
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-06-15 20:27:37 +0700

    change article card

--- frontend/components/wiki/ArticleItem.tsx
+++ frontend/components/wiki/ArticleItem.tsx
@@ -5,10 +5,26 @@ import { ArticleItemProps } from "@/models/wiki";
 
 const ArticleItem: FC<ArticleItemProps> = ({ article, onEdit }) => {
   return (
-    <li className="shadow-md rounded-lg p-5 mb-4 cursor-pointer hover:bg-gray-100 transition-all duration-300" onClick={() => onEdit(article)}>
-      <div>
-        <h3 className="text-lg font-bold mb-2">{article.title}</h3>
-        {article.categories && <p className="text-sm text-gray-600">{article.categories.join(", ")}</p>}
+    <li className="flex flex-col items-start gap-2 rounded-lg border p-4 text-left text-sm transition-all hover:bg-accent bg-[#FFF] cursor-pointer shadow-md" onClick={() => onEdit(article)}>
+      <div className="flex w-full flex-col gap-1 min-w-[250px]">
+        <div className="flex items-center justify-between">
+          <div className="font-semibold text-lg">{article.title}</div>
+        </div>
+        {article.categories && (
+          <div className="flex flex-wrap gap-1 mt-2">
+            {article.categories.map((category) => (
+              <span
+                key={category}
+                className="bg-gray-500 text-white text-xs leading-none rounded-full px-2 py-1"
+              >
+                {category}
+              </span>
+            ))}
+          </div>
+        )}
+      </div>
+      <div className="line-clamp-3 text-xs text-muted-foreground mt-2">
+        {article.content}
       </div>
     </li>
   );
